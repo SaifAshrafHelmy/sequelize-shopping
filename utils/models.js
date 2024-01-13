0dda24872832f2ac6b18a8329c8a60b18ec2fee5
saifashrafhelmy <saifashrafhelmy@yahoo.com>
2024-01-13 14:42:25 +0200

    Practising CRUDING model instances

--- utils/models.js
+++ utils/models.js
@@ -106,5 +106,43 @@ const Payment = sequelize.define(
 (async () => {
   await sequelize.sync();
   console.log('All models were synchronized successfully.');
+  // const customer = Customer.build({
+  //   firstName: 'Saif4',
+  //   lastName: 'Ashraf3',
+  //   phoneNumber: '+201270466650',
+  //   address: 'Cairo, Egypt',
+  // });
+  // customer.set({
+  //   firstName: "Hamada",
+  //   lastName: "Abdo"
+  // })
+  // await customer.save();
+  // await customer.update({firstName: "The greatest customer4"})
+  // console.log(customer.toJSON());
 
+  // const customer = await Customer.findOne();
+  // await customer.destroy();
+  // const product =  Product.build({name: "Laptop M3", price: 1500, quantity: 50, category: "Electronics"})
+  // await product.save({fields: ['name', 'price', 'quantity']})
+  const firstProduct = await Product.findOne({
+    where: {
+      id: 6,
+    },
+  });
+  const prodId = firstProduct.toJSON()['id'];
+
+  await firstProduct.increment({
+    quantity: 3,
+    price: 50,
+  });
+  const firstProductAgain = await Product.findOne({
+    where: {
+      id: prodId,
+    },
+  });
+
+  console.log(firstProductAgain.toJSON());
+
+  // End the connection
+  sequelize.close();
 })();
